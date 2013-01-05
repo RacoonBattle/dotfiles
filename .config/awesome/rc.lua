649757e0cb36fd17bce48b77ff0366c813005fef
Char <charmont.chen@gmail.com>
2013-01-05 15:16:31 +0800

    awesome awful.layout.suit.tile

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -67,28 +67,27 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
---  awful.layout.suit.tile.left,
+  awful.layout.suit.tile,
+  awful.layout.suit.tile.left,
   awful.layout.suit.tile.bottom,
---  awful.layout.suit.tile.top,
---  awful.layout.suit.fair,
+  awful.layout.suit.tile.top,
+  awful.layout.suit.fair,
   awful.layout.suit.fair.horizontal,
 --  awful.layout.suit.spiral,
 --  awful.layout.suit.spiral.dwindle,
---  awful.layout.suit.max,
+  awful.layout.suit.max,
 --  awful.layout.suit.max.fullscreen,
- --   awful.layout.suit.magnifier
+--  awful.layout.suit.magnifier
+  awful.layout.suit.floating,
 }
 -- }}}
 
 -- {{{ Tags
 tags = {
 	names = { " 1 ", " 2 ", " 3 ", " 4 "," 5 " },
-	layout = { layouts[1], layouts[2], layouts[2], layouts[4], layouts[4] }
 }
 for s = 1, screen.count() do
-	tags[s] = awful.tag(tags.names, s, tags.layout)
+	tags[s] = awful.tag(tags.names, s, layouts[1])
 	end
 -- }}}
 
@@ -456,10 +455,6 @@ awful.rules.rules = {
     { rule = { class = "Feh" },
       properties = { floating = true } },
     { rule = { class = "Skype" },
-      properties = { floating = true } },
-    { rule = { class = "Firefox" },
-      properties = { floating = true } },
-    { rule = { class = "Evince" },
       properties = { floating = true } },
     { rule = { class = "Gimp" },
       properties = { floating = true } },
