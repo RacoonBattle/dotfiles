2bef4a0d952a4e6ff34d9d9d33b2223210aea493
zhchen_optiplex-760 <zhchen1991@gmail.com>
2013-02-28 20:52:44 +0800

    company_rc.sh

--- update_config.sh
+++ update_config.sh
@@ -4,8 +4,8 @@
 #	2. some config should be different ( eg: irc and mail config in laptop and company)
 #	   so update these config and apply relevant patch.
 
-# link_files
-home_conf_files=".bashrc .bash_profile .gitconfig .gtcrc-2.0 .gvimrc 
+# link files
+home_conf_files=".bash_profile .gitconfig .gtcrc-2.0 .gvimrc 
 .screenrc .vimperatorrc .vimrc .xinitrc .xmodmaprc .Xresources
 .config/user-dirs.dirs
 .config/ranger/rifle.conf
@@ -19,4 +19,15 @@ for i in $home_conf_files; do
 	ls -l --color=auto ~/$i
 done
 echo "create symlinks done"
+echo 
+
+# copy files
+cp ~/dotfiles/.bashrc ~/.bashrc
+
+# apply patch
+if [ $HOSTNAME = optiplex-760 ];then
+	echo "$HOSTNAME is company's workstation"
+	sh ~/Dropbox/company_rc.sh
+	echo "apply company's patch done"
+fi
 
