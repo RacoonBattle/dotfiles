768c13aa9f847a22c8973e1372b519612ac63537
zhchen@X230 <zhchen1991@gmail.com>
2013-05-21 22:53:40 +0800

    .battery_alarm.sh add logger "Battery warning ..."

--- .battery_alarm.sh
+++ .battery_alarm.sh
@@ -13,8 +13,7 @@ ACTION="sudo shutdown -h now"
 
 MAX_BATTERY=$(cat /proc/acpi/battery/$BAT/info | grep 'last full' | awk '{print$4}')
 
-function get_current_info ()
-{
+function get_current_info() {
 	PRESENT=$(grep "present:" /proc/acpi/battery/$BAT/state | awk '{print $2}')
 	STATE=$(grep "charging state" /proc/acpi/battery/$BAT/state | awk '{print $3}')
 	CAPACITY=$(grep "remaining capacity" /proc/acpi/battery/$BAT/state | awk '{print $3}')
@@ -28,6 +27,7 @@ while [ true ]; do
 
 			if [ $(($PERCENT % 10)) -eq 0 ] && [ "$STATE" = "discharging" ]; then
 				notify-send  -t 5000 "Battery Information" "remaining $PERCENT% "
+				logger "Battery information: remaining capacity $PERCENT%"
 				while [ $(($PERCENT % 10)) -eq 0 ] && [ "$STATE" = "discharging" ]; do
 					get_current_info
 					sleep 60
@@ -37,12 +37,14 @@ while [ true ]; do
 
 			if [ $PERCENT -lt $LOW_BATTERY ] && [ "$STATE" = "discharging" ]; then
 				notify-send -u critical -t 10000 "Battery is LOW." "remaining $PERCENT% "
+				logger "Battery warning: remaining capacity $PERCENT%"
 			fi
 
 			if [ $PERCENT -lt $CRITICAL_BATTERY ] && [ "$STATE" = "discharging" ]; then
 				notify-send -u critical -t 15000 "Battery is less than $CRITICAL_BATTERY." "System have to shutdown"
 				sleep 30
 				notify-send -u critical -t 9000 "LOW Battery" "Power Off" 
+				logger "Battery alarm: remaining capacity $PERCENT%, critical low, pow off system"
 				sleep 9
 				$($ACTION)
 			fi
