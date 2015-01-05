a848348c4d7de508875c83a295ef8a13ee8559f3
Charmont Chen <charmont.chen@gmail.com>
2015-01-05 10:27:35 +0800

    emacs: setting hippie-expand for auto compeletion

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,6 +15,18 @@
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+;;;hippie-expand
+(global-set-key (kbd "M-/") 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-line
+	))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Interface
