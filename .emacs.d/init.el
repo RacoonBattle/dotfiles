803a8238e6d0f2d32fe9f77c0397240a936fc7d6
Charmont Chen <charmont.chen@gmail.com>
2015-01-15 22:07:22 +0800

    emacs: perform tty paste mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,6 +57,33 @@
    (setq interprogram-cut-function 'xsel-cut-function)
    (setq interprogram-paste-function 'xsel-paste-function)))
 
+;; Mimic Vim's set paste
+;; From http://stackoverflow.com/questions/18691973/is-there-a-set-paste-option-in-emacs-to-paste-paste-from-external-clipboard
+(defvar ttypaste-mode nil)
+(add-to-list 'minor-mode-alist '(ttypaste-mode " Paste"))
+(defun ttypaste-mode ()
+  (interactive)
+  (let ((buf (current-buffer))
+	(ttypaste-mode t))
+    (with-temp-buffer
+      (let ((stay t)
+	    (text (current-buffer)))
+	(redisplay)
+	(while stay
+	  (let ((char (let ((inhibit-redisplay t)) (read-event nil t 0.1))))
+	    (unless char
+	      (with-current-buffer buf (insert-buffer-substring text))
+	      (erase-buffer)
+	      (redisplay)
+	      (setq char (read-event nil t)))
+	    (cond
+	     ((not (characterp char)) (setq stay nil))
+	     ((eq char ?\r) (insert ?\n))
+	     ((eq char ?\e)
+	      (if (sit-for 0.1 'nodisp) (setq stay nil) (insert ?\e)))
+	     (t (insert char)))))
+	(insert-buffer-substring text)))))
+
 ;; remove the prompt for killing emacsclient buffers
 (defun server-remove-kill-buffer-hook () (remove-hook
 'kill-buffer-query-functions 'server-kill-buffer-query-function))
@@ -308,3 +335,4 @@
 
 (global-set-key (kbd "<f2>") 'save-buffer)
 (global-set-key (kbd "<f3>") 'linum-mode)
+(global-set-key (kbd "<f4>") 'ttypaste-mode)
