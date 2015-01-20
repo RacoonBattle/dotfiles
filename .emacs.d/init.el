ee90235acc0fb3b69211e00f78db974b14c61797
Charmont Chen <charmont.chen@gmail.com>
2015-01-20 14:06:06 +0800

    emacs: disable sync X clipboard with kill-ring

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,31 +32,6 @@
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-;; Use xsel to access the X clipboard
-;; From https://hugoheden.wordpress.com/2009/03/08/copypaste-with-emacs-in-terminal/
-(unless window-system
- (when (getenv "DISPLAY")
-   ;; Callback for when user cuts
-   (defun xsel-cut-function (text &optional push)
-     ;; Insert text to temp-buffer, and "send" content to xsel stdin
-     (with-temp-buffer
-       (insert text)
-       ;; Use primary the primary selection
-       ;; mouse-select/middle-button-click
-       (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--primary" "--input")))
-   ;; Call back for when user pastes
-   (defun xsel-paste-function()
-     ;; Find out what is current selection by xsel. If it is different
-     ;; from the top of the kill-ring (car kill-ring), then return
-     ;; it. Else, nil is returned, so whatever is in the top of the
-     ;; kill-ring will be used.
-     (let ((xsel-output (shell-command-to-string "xsel --primary --output")))
-       (unless (string= (car kill-ring) xsel-output)
-	 xsel-output)))
-   ;; Attach callbacks to hooks
-   (setq interprogram-cut-function 'xsel-cut-function)
-   (setq interprogram-paste-function 'xsel-paste-function)))
-
 ;; Mimic Vim's set paste
 ;; From http://stackoverflow.com/questions/18691973/is-there-a-set-paste-option-in-emacs-to-paste-paste-from-external-clipboard
 (defvar ttypaste-mode nil)
